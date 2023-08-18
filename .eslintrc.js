ecf7270675d7dd7aa1d186c0e907a64526091628
saad waqas <saadwaqas688@gmail.com>
2023-08-18 10:39:06 +0500

    y

---
+++ .eslintrc.js
@@ -0,0 +1,6 @@
+module.exports = {
+  extends: ['next', 'plugin:prettier/recommended'],
+  rules: {
+    // Add any additional rules or overrides here
+  },
+};
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,11 @@
-import Image from 'next/image'
+import Image from 'next/image';
 
 export default function Home() {
+
+
+
+
+  
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
@@ -109,5 +114,5 @@ export default function Home() {
         </a>
       </div>
     </main>
-  )
+  );
 }
